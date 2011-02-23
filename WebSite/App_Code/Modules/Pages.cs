8b246a0288e577f4e314011639a32dbe1553450f
Yiyi Sun <yiyisun@gmail.com>
2011-02-23 13:01:29 -0500

    add pages module

---
+++ WebSite/App_Code/Modules/Pages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Test
+/// </summary>
+public static class Pages
+{
+	public static void Init()
+	{
+        Site.AddHook("get_homepage", (sender, data) => 
+        {            
+            return "~/Pages/Default";  
+        });
+	}
+}
